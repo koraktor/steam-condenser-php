36ef66b99b87e0cb1c3fd666a7a1f16ec938936f
Sebastian Staudt <koraktor@gmail.com>
2011-12-03 15:28:32 +0100

    Implemented a real test for SteamSocket

---
+++ test/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This code is free software; you can redistribute it and/or modify it under
+ * the terms of the new BSD License.
+ *
+ * Copyright (c) 2011, Sebastian Staudt
+ *
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+
+error_reporting(E_ALL & ~E_USER_NOTICE);
+
+require_once dirname(__FILE__) . "/../lib/steam-condenser.php";
+
+?>
