6f33d39214b78b5ebcc3bdf74b84008dc31c456a
Sebastian Staudt <koraktor@gmail.com>
2012-10-05 07:16:59 +0200

    Always display strict standards errors during tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,12 +3,12 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2011, Sebastian Staudt
+ * Copyright (c) 2011-2012, Sebastian Staudt
  *
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
 
-error_reporting(E_ALL & ~E_USER_NOTICE);
+error_reporting(E_ALL & ~E_USER_NOTICE | E_STRICT);
 
 require_once dirname(__FILE__) . "/../lib/steam-condenser.php";
 
