32003789f9b6384a58c6aaffa731581d116ca193
Sebastian Staudt <koraktor@gmail.com>
2015-08-18 09:05:35 +0200

    Remove S2ALOGSTRINGPacket which is no longer used

--- lib/SteamCondenser/Servers/Packets/SteamPacketFactory.php
+++ lib/SteamCondenser/Servers/Packets/SteamPacketFactory.php
@@ -3,7 +3,7 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2008-2014, Sebastian Staudt
+ * Copyright (c) 2008-2015, Sebastian Staudt
  *
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
@@ -75,9 +75,6 @@ abstract class SteamPacketFactory {
             case SteamPacket::RCON_GOLDSRC_NO_CHALLENGE_HEADER:
             case SteamPacket::RCON_GOLDSRC_RESPONSE_HEADER:
                 return new RCONGoldSrcResponse($data);
-
-            case SteamPacket::S2A_LOGSTRING_HEADER:
-                return new S2ALOGSTRINGPacket($data);
 
             default:
                 throw new PacketFormatException('Unknown packet with header 0x' . dechex($header) . ' received.');
